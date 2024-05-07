c079b69c2f52fd7fea79b02893b3ea1757820049
clear-m <clear-m@ya.ru>
2024-05-07 14:33:51 +0300

    forward sample extended

--- forward.cpp
+++ forward.cpp
@@ -26,6 +26,26 @@ bool wrapper(T &&arg)
     return foo(arg);
 }
 
+// TODO: add tests here
+template <typename T>
+T &&forward(std::remove_reference_t<T> &value)
+{
+    return static_cast<T &&>(value);
+}
+
+template <typename T>
+T &&forward(std::remove_reference<T> &&value)
+{
+    static_assert(!std::is_lvalue_reference_v<T>);
+    return static_cast<T &&>(value);
+}
+
+template <typename T>
+std::remove_reference_t<T> &&move(T &&value)
+{
+    return static_cast<std::remove_reference_t<T> &&>(value);
+}
+
 int main(int argc, char **argv)
 {
     int x = 5;
