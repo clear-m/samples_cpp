835b669511e3cfe1f252c709fe3818a2da06629e
clear-m <clear-m@ya.ru>
2024-06-14 22:46:04 +0300

    allocator sample added

---
+++ allocators.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <memory_resource>
+#include <vector>
+
+template <typename T>
+class Allocator {
+public:
+    using value_type = T;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+
+    template <typename U>
+    struct rebind {
+        using other = Allocator<U>;
+    };
+
+    Allocator() noexcept = default;
+
+    template <typename U>
+    Allocator(const Allocator<U>&) noexcept {}
+
+    [[nodiscard]] pointer allocate(size_type n) {
+        std::cout << "allocate: " << n << std::endl;
+        return static_cast<pointer>(std::malloc(n * sizeof(T)));
+    }
+
+    void deallocate(pointer p, size_type n) noexcept {
+        std::cout << "deallocate: " << n << std::endl;
+        std::free(p);
+    }
+
+    template <typename U, typename... Args>
+    void construct(U* p, Args&&... args) {
+        std::cout << "construct " << std::endl;
+        ::new(static_cast<void*>(p)) U(std::forward<Args>(args)...);
+    }
+
+    void destroy(pointer p) noexcept {
+        std::cout << "destroy " << std::endl;
+        p->~T();
+    }
+};
+
+template <typename T, typename U>
+bool operator==(const Allocator<T>&, const Allocator<U>&) noexcept {
+    return true;
+}
+
+template <typename T, typename U>
+bool operator!=(const Allocator<T>&, const Allocator<U>&) noexcept {
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+    std::vector<int, Allocator<int>> vec(10, 0);
+    return 0;
+}
